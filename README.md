fca96868a8e585e3ad753c5713b29c9051df5f6c
Thomas Debesse <dev@illwieckz.net>
2014-11-10 05:55:23 +0100

    CQB now supported

--- README.md
+++ README.md
@@ -5,13 +5,13 @@ This tool helps user to install TrueCombat mods on GNU/Linux systems.
 
 Only [ET:Legacy](http://etlegacy.com/) and [ioquake3](http://ioquake3.org/) engines are supported since they are the only way to get a working setup in modern distributions.
 
-Current mod supported:
+Current mods supported:
 
+* **TrueCombat:Close Quarter Battle alpha 0.223** for Wolfenstein: Ennemy Territory
 * **TrueCombat:Elite beta 0.4 build 9** for Wolfenstein: Ennemy Territory
 
 To be available:
 
-* **TrueCombat:Close Quarter Battle alpha 0.223** for Wolfenstein: Ennemy Territory
 * **TrueCombat 1.3** for Quake Ⅲ Arena
 
 This script is distributed under the highly permissive [ISC License](COPYING.md).
--- truecombat_installer.sh
+++ truecombat_installer.sh
@@ -1,13 +1,51 @@
 #! /bin/sh
 
+## Args
+#######
+
+mod_name="${1}"
+
+## Vars
+#######
+
 download_dir="/tmp/truecombat_files"
 
 etlegacy_name="Enemy Territory: Legacy"
+etlegacy_install_dir="${HOME}/.etlegacy"
+
+ioquake3_name="ioquake3"
+ioquake3_install_dir="${HOME}/.q3a"
+
+cqb_name="TrueCombat:Close Quarter Battle"
+cqb_mod_name="cqbtest"
+cqb_install_dir="${etlegacy_install_dir}/${cqb_mod_name}"
+cqb_version="alpha 0.223"
+
 tce_name="TrueCombat:Elite"
 tce_mod_name="tcetest"
-
-etlegacy_install_dir="${HOME}/.etlegacy"
 tce_install_dir="${etlegacy_install_dir}/${tce_mod_name}"
+tce_version="beta 0.4 build 9"
+
+tc_name="TrueCombat"
+tc_modname="truecombat"
+tc_install_dir="${ioquake3_install_dir}/${tc_mod_name}"
+tc_version="1.3"
+
+cqb_full_zip_url="http://stealthzone.net/index.php?option=com_docman&task=doc_download&gid=1285&Itemid=17"
+cqb_full_zip_filename="cqb_alpha022_win_linux.zip"
+cqb_full_zip_filepath="${download_dir}/${cqb_full_zip_filename}"
+cqb_full_zip_sum="361b3e0ef8e35a5cb6550a9a8df06f85604d21f898b7c504d24119880add7b46b436f884f550930491f2a6ed071eae8b88678f61dc06e4fdb4efdd8959799aef"
+cqb_full_zip_size="309240933"
+cqb_full_zip_hsize="295M"
+cqb_full_filename_list="autoexec.cfg cgame.mp.i386.so EULA_TrueCombat_CQB.txt mp_bin.pk3 official.dat pak0.pk3 pak1.pk3 pak2.pk3 pak3.pk3 pak4.pk3 qagame.mp.i386.so readme.txt ui.mp.i386.so"
+
+cqb_patch_zip_url="http://stealthzone.net/index.php?option=com_docman&task=doc_download&gid=1287&Itemid=17"
+cqb_patch_zip_filename="cqb_alpha0223_win_linux_patch.zip"
+cqb_patch_zip_filepath="${download_dir}/${cqb_patch_zip_filename}"
+cqb_patch_zip_sum="5fa0c927c34fbffe19a2f7b05cdd361305c7d8b1b00076281e19087716c26e938d4d1e27bea25ba9d74f075ca6ea1766269a4b71f6f7db4764b43d4619804c91"
+cqb_patch_zip_size="10680693"
+cqb_patch_zip_hsize="11M"
+cqb_patch_filename_list="official.dat mp_bin.pk3 pak5.pk3 cgame.mp.i386.so qagame.mp.i386.so ui.mp.i386.so changelog.txt"
 
 tce_full_run_gz_url="http://stealthzone.net/index.php?option=com_docman&task=doc_download&gid=3&Itemid=17"
 tce_full_run_gz_filename="TrueCombatElite_v049_Linux.run.gz"
@@ -38,13 +76,16 @@ tce_patch_zip_filepath="${download_dir}/${tce_patch_zip_filename}"
 tce_patch_zip_sum="8814f193437551c29edf88087229ee813317df10e264c3a0200942ef7c1e876aacff4d985d28bd59d1b3bd9589c86ad034d59a8f434cb443a31e07db9b0562da"
 tce_patch_zip_size="6795533"
 tce_patch_zip_hsize="6.5Mb"
+tce_patch_filename_list="mp_bin.pk3 official.dat pak4.pk3 qagame.mp.i386.so readme_tce0490b.txt ui.mp.i386.so cgame.mp.i386.so"
 
-tce_patch_files="mp_bin.pk3 official.dat pak4.pk3 qagame.mp.i386.so readme_tce0490b.txt ui.mp.i386.so cgame.mp.i386.so"
+## Common Stuff
+###############
 
-
-create_download_dir () {
-	echo "Creating download directory: ${download_dir}"
-	if mkdir "${download_dir}"
+create_dir () {
+	# $1: path
+	# $2: description
+	echo "Creating ${2} directory: ${1}"
+	if mkdir "${1}"
 	then
 		echo "Success"
 		true
@@ -54,11 +95,13 @@ create_download_dir () {
 	fi
 }
 
-check_download_dir () {
-	echo "Verifying ${download_dir} directory: ${download_dir}"
-	if [ -e "${download_dir}" ]
+check_dir () {
+	# $1: path
+	# $2: description
+	echo "Verifying ${2} directory: ${1}"
+	if [ -e "${1}" ]
 	then
-		if [ -d "${download_dir}" ]
+		if [ -d "${1}" ]
 		then
 			echo "Already there"
 			true
@@ -68,13 +111,16 @@ check_download_dir () {
 		fi
 	else
 		echo "Not here, must be created before"
-		create_download_dir
+		create_dir "${1}" "${2}"
 	fi
 }
 
-create_etlegacy_dir () {
-	echo "Creating ${etlegacy_name} directory: ${etlegacy_install_dir}"
-	if mkdir "${etlegacy_install_dir}"
+verify () {
+	# $1: path
+	# $2: sum
+	# $3: description
+	echo "Verifying ${3}: ${1}"
+	if [ "$(sha512sum "${1}" | cut -f1 -d' ')" = "${2}" ]
 	then
 		echo "Success"
 		true
@@ -84,76 +130,21 @@ create_etlegacy_dir () {
 	fi
 }
 
-check_etlegacy_dir () {
-	echo "Verifying ${etlegacy_name} directory: ${etlegacy_install_dir}"
-	if [ -e "${etlegacy_install_dir}" ]
-	then
-		if [ -d "${etlegacy_install_dir}" ]
-		then
-			echo "Already there"
-			true
-		else
-			echo "Error, not a directory"
-			false
-		fi
-	else
-		echo "Not here, must be created before"
-		create_etlegacy_dir
-	fi
-}
-
-create_tce_dir () {
-	echo "Creating ${tce_name} directory: ${tce_install_dir}"
-	if mkdir "${tce_install_dir}"
-	then
-		echo "Success"
-		true
-	else
-		false
-	fi
-}
-
-check_tce_dir () {
-	echo "Verifying ${tce_name} directory: ${tce_install_dir}"
-	if [ -e "${tce_install_dir}" ]
-	then
-		if [ -d "${tce_install_dir}" ]
-		then
-			echo "Already there"
-			true
-		else
-			echo "Error, not a directory"
-			false
-		fi
-	else
-		echo "Not here, must be created before"
-		create_tce_dir
-	fi
-}
-
-verify_tce_full_run_gz () {
-	echo "Verifying ${tce_name} installer archive: ${tce_full_run_gz_filepath}"
-	if [ "$(sha512sum "${tce_full_run_gz_filepath}" | cut -f1 -d' ')" = "${tce_full_run_gz_sum}" ]
-	then
-		echo "Success"
-		true
-	else
-		echo "Failure"
-		false
-	fi
-}
-
-download_tce_full_run_gz () {
-	echo "Downloading ${tce_name} installer archive: ${tce_full_run_gz_filepath} (${tce_full_run_gz_hsize})"
-	if [ -f "${tce_full_run_gz_filepath}" ]
+download () {
+	# $1: url
+	# $2: path
+	# $3: sum
+	# $4: description
+	echo "Downloading ${4}: ${2}"
+	if [ -f "${2}" ]
 	then
 		echo "Already there"
 		true
 	else
-		if wget --quiet "${tce_full_run_gz_url}" -O "${tce_full_run_gz_filepath}"
+		if wget --quiet "${1}" -O "${2}"
 		then
 			echo "Done"
-			verify_tce_full_run_gz
+			true
 		else
 			echo "Failure"
 			false
@@ -161,15 +152,91 @@ download_tce_full_run_gz () {
 	fi
 }
 
-verify_tce_full_run () {
-	echo "Verifying ${tce_name} installer: ${tce_full_run_filepath}"
-	if [ "$(sha512sum "${tce_full_run_filepath}" | cut -f1 -d' ')" = "${tce_full_run_sum}" ]
+check_download_dir () {
+	check_dir "${download_dir}" "download"
+}
+
+## Engine stuff
+###############
+
+check_etlegacy_dir () {
+	check_dir "${etlegacy_install_dir}" "${etlegacy_name}"
+}
+
+check_ioquake3_dir () {
+	check_dir "${ioquake3_install_dir}" "${ioquake3_name}"
+}
+
+## CQB Stuff
+############
+
+check_cqb_dir () {
+	check_dir "${cqb_install_dir}" "${cqb_name}"
+}
+
+download_cqb_full_zip () {
+	if download "${cqb_full_zip_url}" "${cqb_full_zip_filepath}" "${cqb_name} zip archive (${cqb_full_zip_hsize})"
 	then
-		echo "Success"
+		verify "${cqb_full_zip_filepath}" "${cqb_full_zip_sum}" "${cqb_name} zip archive (${cqb_full_zip_hsize})"
+	fi
+}
+
+extract_cqb_full_files () {
+	echo "Extracting ${cqb_name} archive files: ${cqb_full_filename_list}"
+
+	cqb_full_filepath_list=""
+	for file in ${cqb_full_filename_list}
+	do
+		cqb_full_filepath_list="${cqb_full_filepath_list} ${cqb_mod_name}/${file}"
+	done
+
+	if 7z e -y -o"${cqb_install_dir}/." "${cqb_full_zip_filepath}" ${cqb_full_filepath_list} >/dev/null
+	then
+		echo "Done"
 		true
 	else
 		echo "Failure"
 		false
+	fi
+}
+
+download_cqb_patch_zip () {
+	if download "${cqb_patch_zip_url}" "${cqb_patch_zip_filepath}" "${cqb_name} zip archive (${cqb_patch_zip_hsize})"
+	then
+		verify "${cqb_patch_zip_filepath}" "${cqb_patch_zip_sum}" "${cqb_name} zip archive (${cqb_patch_zip_hsize})"
+	fi
+}
+
+extract_cqb_patch_files () {
+	echo "Extracting ${cqb_name} archive files: ${cqb_patch_filename_list}"
+
+	cqb_patch_filepath_list=""
+	for file in ${cqb_patch_filename_list}
+	do
+		cqb_patch_filepath_list="${cqb_patch_filepath_list} ${cqb_mod_name}/${file}"
+	done
+
+	if 7z e -y -o"${cqb_install_dir}/." "${cqb_patch_zip_filepath}" ${cqb_patch_filepath_list} >/dev/null
+	then
+		echo "Done"
+		true
+	else
+		echo "Failure"
+		false
+	fi
+}
+
+## TCE Stuff
+############
+
+check_tce_dir () {
+	check_dir "${tce_install_dir}" "${tce_name}"
+}
+
+download_tce_full_run_gz () {
+	if download "${tce_full_run_gz_url}" "${tce_full_run_gz_filepath}" "${tce_name} installer archive (${tce_full_run_gz_hsize})"
+	then
+		verify "${tce_full_run_gz_filepath}" "${tce_full_run_gz_sum}" "${tce_name} installer archive (${tce_full_run_gz_hsize})"
 	fi
 }
 
@@ -183,25 +250,13 @@ extract_tce_full_run () {
 		if gzip -d < "${tce_full_run_gz_filepath}" > "${tce_full_run_filepath}"
 		then
 			echo "Done"
-			verify_tce_full_run
+			verify "${tce_full_run_filepath}"  "${tce_full_run_sum}" "${tce_name} installer"
 		else
 			echo "Failure"
 			false
 		fi
 	fi
 
-}
-
-verify_tce_full_tar_gz () {
-	echo "Verifying ${tce_name} setup archive: ${tce_full_tar_gz_filepath}"
-	if [ "$(sha512sum "${tce_full_tar_gz_filepath}" | cut -f1 -d' ')" = "${tce_full_tar_gz_sum}" ]
-	then
-		echo "Success"
-		true
-	else
-		echo "Failure"
-		false
-	fi
 }
 
 dump_tce_full_tar_gz () {
@@ -214,7 +269,7 @@ dump_tce_full_tar_gz () {
 		if dd if="${tce_full_run_filepath}" of="${tce_full_tar_gz_filepath}" ibs="${tce_full_tar_gz_offset}" skip=1 obs=1024 status=none
 		then
 			echo "Done"
-			verify_tce_full_tar_gz
+			verify "${tce_full_tar_gz_filepath}" "${tce_full_tar_gz_sum}" "${tce_name} setup archive"
 		else
 			echo "Failure"
 			false
@@ -222,19 +277,7 @@ dump_tce_full_tar_gz () {
 	fi
 }
 
-verify_tce_full_tar_bz2 () {
-	echo "Verifying ${tce_name} archive: ${tce_full_tar_bz2_filepath}"
-	if [ "$(sha512sum "${tce_full_tar_bz2_filepath}" | cut -f1 -d' ')" = "${tce_full_tar_bz2_sum}" ]
-	then
-		echo "Success"
-		true
-	else
-		echo "Failure"
-		false
-	fi
-}
-
-pick_tce_full_tar_bz2_archive () {
+pick_tce_full_tar_bz2 () {
 	echo "Picking ${tce_name} archive: ${tce_full_tar_bz2_filepath}"
 	if [ -f "${tce_full_tar_bz2_filepath}" ]
 	then
@@ -244,7 +287,7 @@ pick_tce_full_tar_bz2_archive () {
 		if tar -xzf "${tce_full_tar_gz_filepath}" "./${tce_full_tar_bz2_filename}" -O > "${tce_full_tar_bz2_filepath}"
 		then
 			echo "Done"
-			verify_tce_full_tar_bz2
+			verify "${tce_full_tar_bz2_filepath}" "${tce_full_tar_bz2_sum}" "${tce_name} archive"
 		else
 			echo "Failure"
 			false
@@ -264,39 +307,16 @@ extract_tce_full_dir () {
 	fi
 }
 
-verify_tce_patch_zip () {
-	echo "Verifying ${tce_name} patch archive: ${tce_patch_zip_filepath}"
-	if [ "$(sha512sum "${tce_patch_zip_filepath}" | cut -f1 -d' ')" = "${tce_patch_zip_sum}" ]
-	then
-		echo "Success"
-		true
-	else
-		echo "Failure"
-		false
-	fi
-}
-
 download_tce_patch_zip () {
-	echo "Downloading ${tce_name} patch archive: ${tce_patch_zip_filepath} (${tce_patch_zip_hsize})"
-	if [ -f "${tce_patch_zip_filepath}" ]
+	if download "${tce_patch_zip_url}" "${tce_patch_zip_filepath}" "${tce_name} patch file (${tce_patch_zip_hsize})"
 	then
-		echo "Already there"
-		true
-	else
-		if wget --quiet "${tce_patch_zip_url}" -O "${tce_patch_zip_filepath}"
-		then
-			echo "Done"
-			verify_tce_patch_zip
-		else
-			echo "Failure"
-			false
-		fi
+		verify "${tce_patch_zip_filepath}" "${tce_patch_zip_sum}" "${tce_name} patch file (${tce_patch_zip_hsize})"
 	fi
 }
 
 extract_tce_patch_files () {
-	echo "Extracting ${tce_name} patch files: ${tce_patch_files}"
-	if 7z e -y -o"${tce_install_dir}/." "${tce_patch_zip_filepath}" ${tce_patch_files} >/dev/null
+	echo "Extracting ${tce_name} patch files: ${tce_patch_filename_list}"
+	if 7z e -y -o"${tce_install_dir}/." "${tce_patch_zip_filepath}" ${tce_patch_filename_list} >/dev/null
 	then
 		echo "Done"
 		true
@@ -306,8 +326,27 @@ extract_tce_patch_files () {
 	fi
 }
 
-main () {
+## TrueCombat Stuff
+###################
 
+check_tc_dir () {
+	check_dir "${tc_install_dir}" "${tc_name}"
+}
+
+## Installation
+###############
+
+install_cqb () {
+	check_download_dir \
+	&& check_etlegacy_dir \
+	&& check_cqb_dir \
+	&& download_cqb_full_zip \
+	&& download_cqb_patch_zip \
+	&& extract_cqb_full_files \
+	&& extract_cqb_patch_files
+}
+
+install_tce () {
 	check_download_dir \
 	&& check_etlegacy_dir \
 	&& check_tce_dir \
@@ -315,9 +354,47 @@ main () {
 	&& download_tce_patch_zip \
 	&& extract_tce_full_run \
 	&& dump_tce_full_tar_gz \
-	&& pick_tce_full_tar_bz2_archive \
+	&& pick_tce_full_tar_bz2 \
 	&& extract_tce_full_dir \
 	&& extract_tce_patch_files
+}
+
+install_tc () {
+	check_download_dir \
+	&& check_ioquake3_dir \
+	&& check_tc_dir \
+	echo "STUB"
+}
+
+main () {
+	case "${mod_name}" in
+		"cqb")
+			echo "Will install ${cqb_name} ${cqb_version}."
+			install_cqb
+			;;
+		"tce")
+			echo "Will install ${tce_name} ${tce_version}."
+			install_tce
+			;;
+		"tc")
+			echo "Will install ${tc_name} ${tc_version}."
+			install_tc
+			;;
+		"all")
+			echo "Will install ${cqb_name} ${cqb_version}, ${tce_name} ${tce_version} and ${tc_name} ${tc_version}."
+			install_cqb
+			install_tce
+			install_tc
+			;;
+		""|"default")
+			echo "Will install ${cqb_name} ${cqb_version} by default."
+			install_cqb
+			;;
+		*)
+			echo "Bad argument"
+			false
+			;;
+	esac
 
 }
 
